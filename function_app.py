a5cb39547816cbbcba0d849cbae8b1908c27bdf4
marco <marco.cambon@gmail.com>
2025-01-21 23:09:06 +0100

    test timer

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ import os
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="30 3 * * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="0 10 23 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def pattodatebotfunction(myTimer: func.TimerRequest) -> None:
     common_main()
